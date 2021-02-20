63d721c248cf2a36185844bdd96a8f9b6ccff8d7
Kouta Takamatsu <bb1016181@gmail.com>
2021-02-20 23:49:31 +0900

    現在時刻との比較結果の表示部分の追加

--- app/src/main/java/com/kouta/scheduleapplication/util/BindingAdapters.kt
+++ app/src/main/java/com/kouta/scheduleapplication/util/BindingAdapters.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.util.Log
 import android.widget.Spinner
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.kouta.scheduleapplication.R
 import com.kouta.scheduleapplication.model.Schedule
@@ -24,4 +25,22 @@ fun setTimeText(view: TextView, time: Schedule.TimeStamp.Time?) {
 @BindingAdapter("android:setDateText")
 fun setDateText(view: TextView, date: Schedule.TimeStamp.Date?) {
     view.text = "${date?.year}年${date?.month}月${date?.day}日(${date?.dayOfWeek})"
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("android:setDeadlineText")
+fun setDeadlineText(view: TextView, timestamp: Schedule.TimeStamp?) {
+    if (timestamp != null) {
+        if (timestamp.date == null) {
+            view.text = "あと${timestamp.time?.hour}時間${timestamp.time?.minute}分"
+        } else {
+            view.text = "あと${timestamp.date.day}日${timestamp.time?.hour}時間"
+        }
+        val color = ContextCompat.getColor(view.context, R.color.black)
+        view.setTextColor(color)
+    } else {
+        view.text = "予定の期限を過ぎています。"
+        val color = ContextCompat.getColor(view.context, R.color.md_red_100)
+        view.setTextColor(color)
+    }
 }
